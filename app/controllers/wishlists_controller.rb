11452cd2b71ab6d2fa269860c0f4478fcc20b4f6
Andy Leverenz <andy@justalever.com>
2024-06-01 17:33:05 -0500

    Add Hotwire wishlists features

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -3,24 +3,27 @@ class WishlistsController < ApplicationController
 
   def create
     @property = Property.find(params[:property_id])
+
     unless current_user.properties.include?(@property)
       current_user.properties << @property
-      flash[:notice] = "Property added to wishlist successfully."
-    else
-      flash[:alert] = "Property is already in the wishlist."
     end
-    redirect_back fallback_location: root_path
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: root_path }
+    end
   end
 
   def destroy
-
     @property = Property.find(params[:id])
+
     if current_user.properties.include?(@property)
       current_user.properties.destroy(@property)
-      flash[:notice] = "Property removed from wishlist successfully."
-    else
-      flash[:alert] = "Property is not in the wishlist."
     end
-    redirect_back fallback_location: root_path
+
+     respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_back fallback_location: root_path }
+    end
   end
 end
